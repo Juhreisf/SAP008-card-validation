05bfdce1227df76bb627844a65ecd5308bee6345
JulianaReis <juliana.rfernandes12@gmail.com>
2022-08-01 15:24:50 -0300

    Commit1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import validator from './validator.js';
 
-console.log(validator);
+let btnCard = document.querySelector("#btnValid");
+let respCard = document.querySelector("#outCard");
+
+btnCard.addEventListener('click', validadorCard);
+
+function validadorCard() {
+    let numCard = document.querySelector("#nCartao").value; // parseInt()
+
+    let resultMasky = validator.maskify(numCard);
+    
+    let resultadoFinal = validator.isValid(numCard); // argumento
+
+    if (numCard === "") {
+        return respCard.innerText = "Digite o número do cartão";
+    }
+    else if (resultadoFinal == true) {
+       
+        alert(resultMasky + "Parabéns, pagamento efetuado com sucesso")
+    
+    }
+    else {
+        
+        alert(resultMasky + " Insira o cartão novamente, erro no cartão")
+    
+
+        
+    } 
+
+}
+
+
+     
+
+
+
+
+
+
+
+
+
